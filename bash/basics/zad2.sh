90c0167ce6e2b8d1caae10fb2d50c2622105a255
mmarkowski321 <65710312+mmarkowski321@users.noreply.github.com>
2024-12-03 21:10:00 +0100

    Update zad2.sh
    Updated time of working

--- bash/basics/zad2.sh
+++ bash/basics/zad2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#2. Memory Usage Monitoring (with background execution for 20 minutes)
+#2. Memory Usage Monitoring (with background execution for 5 minutes)
 #Write a script that:
 # - Displays the current memory usage on the system (you can use free or top).
 # - Saves the output to a log file.
-# - Runs in the background for 20 minutes, saving the memory usage every minute.
+# - Runs in the background for 5 minutes, saving the memory usage every minute.
 
 
 timeout 300 bash -c 'while true; do free -h >> memoryused.txt; sleep 60; done' &
